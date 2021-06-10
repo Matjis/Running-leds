c33b00297b861d2f2f3a6c6dde3f7b1d4cb2f29a
Matjis <mvaivodins@gmail.com>
2021-06-10 23:44:47 +0300

    Direction variable is changed to bool type. Code supplemented with comments. Main loop function set with correct LED movement setup.

--- main.c
+++ main.c
@@ -6,10 +6,9 @@
 #define RCC_AHB2_ENR_ADDR  						(RCC_BASE_ADDR + RCC_AHB2_ENR_OFFSET)
 #define RCC_APB2_ENR_OFFSET						0x60UL
 #define RCC_APB2_ENR_ADDR  						(RCC_BASE_ADDR + RCC_APB2_ENR_OFFSET)
-//*(volatile uint32_t *)
 
 #define	APB1PERIPH_BASE_ADDR					0x48000000UL
-#define GPIOA_BASE_ADDR							APB1PERIPH_BASE_ADDR // led2 uses pa5 pin and has to be output to supply current
+#define GPIOA_BASE_ADDR							APB1PERIPH_BASE_ADDR 
 #define GPIOA_MODE_REG 							(GPIOA_BASE_ADDR + 0x00)
 #define GPIOA_SPEED_REG 						(GPIOA_BASE_ADDR + 0x08)
 #define GPIOA_PUPD_REG 							(GPIOA_BASE_ADDR + 0x0C)
@@ -31,30 +30,14 @@
 #define BuiltInLED								5
 #define ExtButton								7
 
-int dir = 0;
 
-static int leds[6]={6,12,11,9,8,10}; // 
+static int leds[6] = {6,12,11,9,8,10}; // array for LED pin numbers 
+static _Bool dir = 1; // variable for LED direction
+
 
 void delay(void){
 	for(uint32_t i=0; i < 50000 ; i++);
 }
-
-void leds_run(uint32_t *GPIOA_OUTPUTDATA ){
-	if(dir == 1)
-		for(int i=6; i>=0 ; i--){
-			// set GPIOA pin 6 to on/off with delay for output LED
-			*GPIOA_OUTPUTDATA ^= ( 1 << leds[i]) ;
-			delay();
-		}
-	
-	else
-		for(int i=0; i<6 ; i++){
-			// set GPIOA pin 6 to on/off with delay for output LED
-			*GPIOA_OUTPUTDATA ^= ( 1 << leds[i]) ;
-			delay();
-		}
-
-}  
 
 int main(void)
 {
@@ -62,11 +45,14 @@ int main(void)
 	uint32_t *pRccAhb2Enr = (uint32_t*) (RCC_AHB2_ENR_ADDR);
 	*pRccAhb2Enr |= (1 << 0);
 
+	// GPIOA port register pointers
 	uint32_t *GPIOA_MDOE = (uint32_t*) (GPIOA_MODE_REG);
 	uint32_t *GPIOA_SPEED = (uint32_t*) (GPIOA_SPEED_REG );
 	uint32_t *GPIOA_PUPD = (uint32_t*) (GPIOA_PUPD_REG);
 	uint32_t *GPIOA_OPTYPE = (uint32_t*) (GPIOA_OPTYPE_REG );
+	uint32_t *GPIOA_OUTPUTDATA = (uint32_t*) (GPIOA_OUTPUTDATA_REG );
 
+	// set registers for each LED
 	for(int i=0; i<6 ; i++){
 		
 		// Clean and set GPIOA mode register to output
@@ -85,12 +71,11 @@ int main(void)
 		*GPIOA_OPTYPE &= ~( 1 << leds[i] );
 	}
 
-	// GPIOA output data register pointer
-	uint32_t *GPIOA_OUTPUTDATA = (uint32_t*) (GPIOA_OUTPUTDATA_REG );
 
-	
-	// Button interrupt configuration
-	
+	/* 
+	Button and interrupt configuration
+	*/
+
 	//Clean and set GPIOA mode register to input
 	*GPIOA_MDOE &= ~( 3 << ( ExtButton *2) );
 	
@@ -106,7 +91,7 @@ int main(void)
 	uint32_t *pRccApb2Enr = (uint32_t*) (RCC_APB2_ENR_ADDR);
 	*pRccApb2Enr |= (1 << 0);
 
-	//Configure GPIO port selection
+	//Configure GPIO port selection - for GPIO port A isn't needed, for other ports must be set
 	//uint32_t *SYSCFG_EXTICR1 = (uint32_t*) (EXTI_RTSR1_REG);
 	//*SYSCFG_EXTICR1 |= (0 << ExtButton %4);
 
@@ -125,22 +110,37 @@ int main(void)
 	//IRQ setup
 	*NVIC_ISER0 |= (1 << IRQ_NO_EXTI9_5) ;
 
-
+	int i = 0; // variable for move through LED array
 
 	while(1){
-		leds_run(GPIOA_OUTPUTDATA);
-	
+
+		// turn specific LED on, wait and turn it off
+		*GPIOA_OUTPUTDATA ^= ( 1 << leds[i]) ;
+		delay();
+		*GPIOA_OUTPUTDATA ^= ( 1 << leds[i]) ;
+
+		// make changes to direction in witch LEDs turn on and off
+		if (dir == 0)
+			i++;
+		else if (dir == 1)
+			i--;
+
+		// ensure that only defined LEDs from an array are used
+		if (i < 0)
+			i = 6;
+		else if (i > 6)
+			i = 0;
 	}
 }
 
 void EXTI9_5_IRQHandler(void){
-	delay();
 
 	// Clean pending interrupt flag register
 	uint32_t *EXTI_PR1 = (uint32_t*) (EXTI_PR1_REG);
 	if(*EXTI_PR1 & ( 1 << ExtButton))
 		*EXTI_PR1 |= ( 1 << ExtButton);
 	
+	//change variable used for LED direction
 	dir ^= 1;
 	
 }
